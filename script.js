dbc0a5a9a2b45a00ea3d3e907652a1be660de98b
Lance Toledo <lanceatoledo@gmail.com>
2021-10-05 18:24:44 -0400

    Added script.js and media queries

---
+++ script.js
@@ -0,0 +1,18 @@
+// Questions?
+
+// How do we access the user's input?
+let input = document.getElementsByClassName("input_container")[0]
+console.log(input)
+
+input.addEventListener("keyup", function() {
+    console.log(input.value)
+})
+
+
+// How can we store the value of the bill?
+// How can we write a function that calculates the tip amount based on bill and tip button selected
+
+// How can we target tip amount and update it to the correct amount
+// How can we target total and update the display
+
+// Function that give the total amount per person
